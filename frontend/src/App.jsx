306ce140c76b8ddd1ae9fe24e0eac8347a702e7a
vivek-gamit <vgamit637@gmail.com>
2026-02-02 15:58:28 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline text-blue-600'>
+      Tailwind test
+    </div>
+  )
+}
+
+export default App
